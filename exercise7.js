da5591b7ad2e042e001c7ffafb91138f1492ccac
Ed Hazledine <ed.hazledine@gmail.com>
2018-09-29 15:46:36 +0100

    Exercise 7 completed

---
+++ exercise7.js
@@ -0,0 +1,7 @@
+let http = require('http');
+
+http.get(process.argv[2], function (response) {
+    response.setEncoding('utf8');
+    response.on('error', console.error);
+    response.on('data', console.log);
+}).on('error', console.error);
