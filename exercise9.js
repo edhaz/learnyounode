dcc35c5ed700276f08b5297627d08fdf5e5dd449
Ed Hazledine <ed.hazledine@gmail.com>
2018-10-05 17:40:28 +0100

    Exercise 9 complete

--- exercise9.js
+++ exercise9.js
@@ -1,14 +1,25 @@
 let bl = require('bl');
 let http = require('http');
 
-for (let i = 2; i <= 4; i++){
-    http.get(process.argv[i], response => {
-        response.pipe(bl(function (err, data) {
-            if (err) {
-                return console.error(err)
-            }
-            const dataString = data.toString();
-            console.log(dataString);
-        }))
+function thisFunc(url) {
+    return new Promise(resolve => {
+        http.get(url, response => {
+            response.pipe(bl(function (err, data) {
+                if (err) {
+                    return console.error(err)
+                }
+                resolve(data.toString());
+            }))
+        });
     });
 }
+
+async function asyncCall() {
+    for (let i = 2; i < 5; i++) {
+        let url = process.argv[i];
+        const result = await thisFunc(url);
+        console.log(result);
+    }
+}
+
+asyncCall();
