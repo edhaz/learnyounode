777945dd4dfc72e5321c9055a2f9137eff90ebff
Ed Hazledine <ed.hazledine@gmail.com>
2018-10-06 10:33:49 +0100

    Exercise 10 completed

--- exercise10.js
+++ exercise10.js
@@ -4,9 +4,33 @@ let strftime = require('strftime');
 let portNumber = process.argv[2];
 
 let server = net.createServer(function (socket) {
-    let data = `${strftime('%F %T')}\n`;
+    let data = `${strftime('%F %H:%M')}\n`;
     socket.write(data);
     socket.end();
 });
 server.listen(portNumber);
 
+
+/* OFFICIAL
+var net = require('net')
+
+    function zeroFill (i) {
+      return (i < 10 ? '0' : '') + i
+    }
+
+    function now () {
+      var d = new Date()
+      return d.getFullYear() + '-' +
+        zeroFill(d.getMonth() + 1) + '-' +
+        zeroFill(d.getDate()) + ' ' +
+        zeroFill(d.getHours()) + ':' +
+        zeroFill(d.getMinutes())
+    }
+
+    var server = net.createServer(function (socket) {
+      socket.end(now() + '\n')
+    })
+
+    server.listen(Number(process.argv[2]))
+
+ */
