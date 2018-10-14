35aeb5296c139123902c02a93c270b8cca128c3e
Ed Hazledine <ed.hazledine@gmail.com>
2018-10-14 12:35:39 +0100

    Exercise 11 complete

--- exercise11.js
+++ exercise11.js
@@ -3,9 +3,27 @@ let fs = require('fs');
 let portNumber = process.argv[2];
 
 let server = http.createServer(function (req, res) {
-    dat = fs.createReadStream(req);
-    console.log(dat);
-});
-server.listen(portNumber);
+    let filename = process.argv[3];
+    let readStream = fs.createReadStream(filename);
+    readStream.on('open', function () {
+        readStream.pipe(res);
+    });
 
-// fs.createReadStream()
+    readStream.on('error', function(err) {
+        res.end(err);
+    })
+}).listen(portNumber);
+
+
+// OFFICIAL VERSION
+//
+// var http = require('http');
+// var fs = require('fs');
+//
+// var server = http.createServer(function (req, res) {
+//     res.writeHead(200, { 'content-type': 'text/plain' });
+//
+//     fs.createReadStream(process.argv[3]).pipe(res);
+// });
+//
+// server.listen(Number(process.argv[2]));
